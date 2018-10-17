0f09481f62cd428eabd7d46abaeb652983e1778c
Greg <gregoryc@outlook.com>
2018-10-17 16:29:23 -0700

    initial commit

---
+++ viral.py
@@ -0,0 +1,51 @@
+from tkinter import Tk, Label, Button
+
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("A simple GUI")
+
+        self.label = Label(master, text="This isn't our first GUI!")
+        self.label.pack()
+
+        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button.pack()
+
+        self.change_button = Button(master, text="Change", command=self.change_label)
+        self.change_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    def greet(self):
+        print("Greetings!")
+
+    def change_label(self):
+        self.label.config(text="New text")
+
+root = Tk()
+my_gui = MyFirstGUI(root)
+root.mainloop()
+
+
+'''
+import tkinter as tk
+counter = 0 
+def counter_label(label):
+  def count():
+    global counter
+    counter += 1
+    label.config(text=str(counter))
+    label.after(1000, count)
+  count()
+ 
+ 
+root = tk.Tk()
+root.title("Counting Seconds")
+label = tk.Label(root, fg="green")
+label.pack()
+counter_label(label)
+button = tk.Button(root, text='Stop', width=25, command=root.destroy)
+button.pack()
+root.mainloop()
+'''
