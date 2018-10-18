7dd059ca32ab583354916ebdce31d4ffe4bb5c91
Greg <gregoryc@outlook.com>
2018-10-17 17:20:55 -0700

    finished session

---
+++ brian.py
@@ -0,0 +1,26 @@
+import numpy as np
+import random
+
+def getcard(catnames,inp,truths,lies):
+
+    cats = np.random.choice(4,3,replace=False)
+    print(cats)
+    np.random.shuffle(cats)
+
+    #Pick a truth
+    show1 = catnames[cats[0]] + ' = ' + truths[cats[0]]
+    #Pick a lie
+    show2 = catnames[cats[1]] + ' = ' + np.random.choice(lies[cats[1]])
+    #Pick an unknown
+    if random.choice([True, False]):
+        show3 = catnames[cats[2]] + ' = ' + truths[cats[2]]
+    else:
+        show3 = catnames[cats[2]] + ' = ' + np.random.choice(lies[cats[2]])
+    print("show1", show1)
+    print("show2", show2)
+    print("show3", show3)
+    show = [show1,show2,show3]
+    print("show", show)
+    np.random.shuffle(show)
+    print("ret", show)
+    return show
--- viral.py
+++ viral.py
@@ -1,12 +1,50 @@
-from tkinter import Tk, Label, Button
+from brian import *
+from tkinter import *
+import random
+
+cat_names = []
+inp = []
+truths = []
+lies = []
+
+
+for i in range(4):
+    newname = input("Input a category name: ")
+    cat_names.append(newname)
+
+for i in range(4):
+    catname = cat_names[i]
+    cur_lies = []
+    cur_inp = []
+    for i in range(3):
+        opt = input("Enter an option for category \"" + catname + "\": ")
+        cur_lies.append(opt)
+        cur_inp.append(opt)
+    cur_truth = random.choice(cur_lies)
+    truths.append(cur_truth)
+    cur_lies.remove(cur_truth)
+    lies.append(cur_lies)
+    inp.append(cur_inp)
+
+print("catnames", cat_names)
+print("truths", truths)
+print("lies", lies)
+print("input", inp)
+
+cards = []
+for i in range(100):
+    cards.append(getcard(cat_names, inp, truths, lies))
+
+
 
 class MyFirstGUI:
     def __init__(self, master):
         self.master = master
+        self.i = 0
         master.title("A simple GUI")
 
         self.label = Label(master, text="This isn't our first GUI!")
-        self.label.pack()
+        self.label.pack(pady=25)
 
         self.greet_button = Button(master, text="Greet", command=self.greet)
         self.greet_button.pack()
@@ -17,16 +55,68 @@ class MyFirstGUI:
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.pack()
 
+        self.bot_label = Label(master, text="Made by XX")
+        self.bot_label.pack(pady=100)
+
     def greet(self):
         print("Greetings!")
 
     def change_label(self):
-        self.label.config(text="New text")
+        self.label.config(text=cards[self.i])
+        self.i += 1
 
 root = Tk()
+
+root.title('The game')
+#You can set the geometry attribute to change the root windows size
+root.geometry("500x500") #You want the size of the app to be 500x500
+root.resizable(0, 0)
+back = Frame(master=root,bg='grey')
+back.pack_propagate(0) #Don't allow the widgets inside to determine the frame's width / height
+back.pack(fill=BOTH, expand=1)
+
 my_gui = MyFirstGUI(root)
 root.mainloop()
 
+'''
+
+
+
+
+
+import tkinter as tk
+
+def startgame():
+    pass
+
+mw = tk.Tk()
+
+#If you have a large number of widgets, like it looks like you will for your
+#game you can specify the attributes for all widgets simply like this.
+mw.option_add("*Button.Background", "black")
+mw.option_add("*Button.Foreground", "red")
+
+mw.title('The game')
+#You can set the geometry attribute to change the root windows size
+mw.geometry("500x500") #You want the size of the app to be 500x500
+mw.resizable(0, 0) #Don't allow resizing in the x or y direction
+
+back = tk.Frame(master=mw,bg='black')
+back.pack_propagate(0) #Don't allow the widgets inside to determine the frame's width / height
+back.pack(fill=tk.BOTH, expand=1) #Expand the frame to fill the root window
+
+#Changed variables so you don't have these set to None from .pack()
+go = tk.Button(master=back, text='Start Game', command=startgame)
+go.pack()
+close = tk.Button(master=back, text='Quit', command=mw.destroy)
+close.pack()
+info = tk.Label(master=back, text='Made by me!', bg='red', fg='black')
+info.pack()
+
+mw.mainloop()
+
+
+'''
 
 '''
 import tkinter as tk
